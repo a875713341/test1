6d3de1067b43eabcb1f6b842a86e304c89efaa51
Administrator <Administrator@PC-20171115ONGG>
2020-01-04 20:28:11 +0800

    user

--- _test1/src/_test1/Test1.java
+++ _test1/src/_test1/Test1.java
@@ -4,7 +4,9 @@ public class Test1 {
 	int a=1;
 public static void main(String[] args) {
 	int a=2;
+	String b="abc";
 	System.out.println("java");
 	System.out.println("dong");
+	System.out.println(b);
 }
 }
